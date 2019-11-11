ce9eaaff9da814852550a6085b28565ff3a0a909
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-11-11 10:15:29 +0530

    Update routes.js

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -12,6 +12,29 @@ module.exports = (app, db) => {
     //     res.status(200).send('Bon Jour');
         
     // });
+    app.post("/signup", (req, res) => {
+        console.log(req.body);
+        const note = { name: req.body.name, email:req.body.email,password:req.body.password };
+    
+        db.collection('UserData').findOne({email: req.body.email}).then(function(result){
+            if(!(result==null)){
+                res.status(200).json({msg:"Email Id already present"});
+            }
+            else
+            {
+                db.collection('UserData').insertOne(note, (err, result) => {
+                    if (err)
+                    {
+                        console.log(err + " this error has occured");
+                    }
+                    else
+                    {
+                         res.status(200).json({msg:"success"});
+                    }
+                });
+             }
+       });
+    });
 
     app.get("/", (req, res) => {
 
