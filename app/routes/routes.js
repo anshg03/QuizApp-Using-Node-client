ad1e6bae804f92a1fa2a5cd5deed96d5f051ad44
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-11-11 14:26:34 +0530

    Update routes.js

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -23,6 +23,34 @@ module.exports = (app, db) => {
        });
     });
 
+    app.post("/", (req, res) => {
+        const note = { email: req.body.email, password: req.body.password };
+        //console.log(req.body);
+        db.collection('UserData').findOne({email: req.body.email, password: req.body.password}).then(function(result){
+            if(!(result==null)){
+            
+                db.collection('UserData').findOne(note, (err, result) => {
+                    if (err)
+                    {
+                        console.log(err + " this error has occured");
+                        
+                    }
+                    else
+                    {
+                        console.log(result);
+                        userData=result;
+                        res.status(200).json({msg:"User Exist"});
+                    }
+                });
+            
+            }
+           else
+           {
+              res.status(200).json({msg:"User Does Not Exist"});
+           }
+        });
+    });
+
     app.get("/", (req, res) => {
 
         db.collection('QueSet').find({}).toArray(function (err, result) {
